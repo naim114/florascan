ab0f962d97b61fbee94827cd62abc58a337f371c
naim114 <naim.muhammad99@gmail.com>
2023-12-11 14:16:07 +0800

    Create wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:florascan/src/services/helpers.dart';
 import 'package:florascan/src/theme/theme_mode_manager.dart';
+import 'package:florascan/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
