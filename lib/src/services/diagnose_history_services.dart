d53facc4e25485a51c29961f0023756c0b99b0e1
naim114 <naim.muhammad99@gmail.com>
2024-05-22 13:02:58 +0800

    Add CRUD method to DiagnoseHistoryServices

--- lib/src/services/diagnose_history_services.dart
+++ lib/src/services/diagnose_history_services.dart
@@ -1,14 +1,22 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:florascan/src/models/diagnose_history_model.dart';
+import 'package:florascan/src/models/plant_disease_model.dart';
 import 'package:florascan/src/services/plant_services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:path/path.dart' as path;
 
+import '../models/user_model.dart';
 import 'user_services.dart';
 
 class DiagnoseHistoryServices {
   final CollectionReference _collectionRef =
       FirebaseFirestore.instance.collection('DiagnoseHistory');
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
 
   // convert DocumentSnapshot to model object
   Future<DiagnoseHistoryModel?> fromDocumentSnapshot(
@@ -120,5 +128,115 @@ class DiagnoseHistoryServices {
     }
 
     return dataList;
+  }
+
+  Future add({
+    required PlantDiseaseModel disease,
+    required UserModel user,
+    required String description,
+    required File imageFile,
+  }) async {
+    try {
+      dynamic add = await _collectionRef.add({
+        'id': null,
+        'user': null,
+        'dateTime': null,
+        'disease': null,
+        'imgPath': null,
+        'imgURL': null,
+      }).then((docRef) async {
+        // UPLOAD TO FIREBASE STORAGE
+        // Get file extension
+        String extension = path.extension(imageFile.path);
+        print("Extension: $extension");
+
+        // Create the file metadata
+        final metadata = SettableMetadata(contentType: "image/jpeg");
+
+        // Create a reference to the file path in Firebase Storage
+        final storageRef = _firebaseStorage
+            .ref()
+            .child('diagnose_history/${user.id}/${docRef.id}$extension');
+
+        // Upload the file to Firebase Storage
+        final uploadTask = storageRef.putFile(imageFile, metadata);
+
+        uploadTask.snapshotEvents.listen((TaskSnapshot taskSnapshot) {
+          switch (taskSnapshot.state) {
+            case TaskState.running:
+              final progress = 100.0 *
+                  (taskSnapshot.bytesTransferred / taskSnapshot.totalBytes);
+              print("Upload is $progress% complete.");
+              break;
+            case TaskState.paused:
+              print("Upload is paused.");
+              break;
+            case TaskState.canceled:
+              print("Upload was canceled");
+              break;
+            case TaskState.error:
+              // Handle unsuccessful uploads
+              print("Upload encounter error");
+              throw Exception();
+            case TaskState.success:
+              // Handle successful uploads on complete
+              print("News thumbnail uploaded");
+              break;
+          }
+        });
+
+        // Get the download URL of the uploaded file
+        final downloadUrl =
+            await uploadTask.then((TaskSnapshot taskSnapshot) async {
+          final url = await taskSnapshot.ref.getDownloadURL();
+          return url;
+        });
+
+        print("URL: $downloadUrl");
+
+        // UPDATE ON FIRESTORE
+        _collectionRef
+            .doc(docRef.id)
+            .set(DiagnoseHistoryModel(
+              id: docRef.id,
+              user: user,
+              dateTime: DateTime.now(),
+              disease: disease,
+              imgPath: 'diagnose_history/${user.id}/${docRef.id}$extension',
+              imgURL: downloadUrl,
+            ).toJson())
+            .then((value) => print("News Added"));
+      });
+
+      print("Add News: $add");
+
+      return true;
+    } catch (e) {
+      print(e.toString());
+      Fluttertoast.showToast(msg: e.toString());
+      return false;
+    }
+  }
+
+  Future delete({
+    required DiagnoseHistoryModel diagnoseHistory,
+  }) async {
+    try {
+      // delete previous file
+      final Reference ref =
+          _firebaseStorage.ref().child(diagnoseHistory.imgPath!);
+      await ref.delete();
+
+      final delete = _collectionRef.doc(diagnoseHistory.id).delete();
+
+      print("Delete Diagnose History: $delete");
+
+      return true;
+    } catch (e) {
+      print(e.toString());
+      Fluttertoast.showToast(msg: e.toString());
+
+      return false;
+    }
   }
 }
